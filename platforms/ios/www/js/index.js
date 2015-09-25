64bd52bacc389c25bd3e13b16aea99bd79af1604
Marvin Xue <bsetsu@asx.co.jp>
2015-09-25 18:36:41 +0900

    enhance some feature and ui

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		
     },
     // deviceready Event Handler
     //
@@ -79,8 +80,29 @@ var app = {
 			document.getElementById("datareceived").value = document.getElementById("datareceived").value+"\n"+pcEpcString;
 		}
 		var rfidPcEpcDataReceived = function(pcEpcData){
-			alert(String.fromCharCode.apply(null, new Uint16Array(pcEpcData)));
+			//alert(String.fromCharCode.apply(null, new Uint16Array(pcEpcData)));
 			//document.getElementById("datareceived").value = document.getElementById("datareceived").value+"\n"+pcEpcString;
+		}
+		var barcodePowerListener = function(power){
+			
+			//alert(String.fromCharCode.apply(null, new Uint16Array(pcEpcData)));
+			document.getElementById("datareceived").value = document.getElementById("datareceived").value+"\nBARCODE POWER:"+power;
+			if(power =='ON'){
+				document.getElementById("deviceready").querySelector('.asr_bc_poweron').setAttribute('style', 'display:block;');
+			}else{
+				document.getElementById("deviceready").querySelector('.asr_bc_poweron').setAttribute('style', 'display:none;');
+			}
+		}
+
+		var rfidPowerListener = function(power){
+			
+			//alert(String.fromCharCode.apply(null, new Uint16Array(pcEpcData)));
+			document.getElementById("datareceived").value = document.getElementById("datareceived").value+"\nRFID POWER:"+power;
+			if(power =='ON'){
+				document.getElementById("deviceready").querySelector('.asr_rf_poweron').setAttribute('style', 'display:block;');
+			}else{
+				document.getElementById("deviceready").querySelector('.asr_rf_poweron').setAttribute('style', 'display:none;');
+			}
 		}
 		//asreader.powerOn("ON", success, failure);
 		asreader.setBarcodePluggedListener(barcodeReaderPlugged);
@@ -89,6 +111,24 @@ var app = {
 		asreader.setRfidPcEpcStringListener(rfidPcEpcStringReceived);
 
 		asreader.setRfidPcEpcDataListener(rfidPcEpcDataReceived);
+
+		asreader.setBarcodePowerListener(barcodePowerListener);
+		asreader.setRfidPowerListener(rfidPowerListener);
+		
+		document.getElementById("barcodePowerOn").addEventListener('click',function(){asreader.barcodePowerOn(null,null);});
+		document.getElementById("barcodePowerOff").addEventListener('click',function(){asreader.barcodePowerOff(null,null);});
+		document.getElementById("readBarcode").addEventListener('click',function(){asreader.readBarcode(null,null);});
+		//document.getElementById("readBarcodeContinuously").addEventListener('click',function(){asreader.readBarcodeContinuously(null,null);});
+		document.getElementById("stopReadBarcode").addEventListener('click',function(){asreader.stopReadBarcode(null,null);});
+		
+		document.getElementById("rfidPowerOn").addEventListener('click',function(){asreader.rfidPowerOn(null,null);});
+		document.getElementById("rfidPowerOff").addEventListener('click',function(){asreader.rfidPowerOff(null,null);});
+		document.getElementById("startReadTags").addEventListener('click',function(){asreader.startReadTags(null,null);});
+		document.getElementById("stopReadTags").addEventListener('click',function(){asreader.stopReadTags(null,null);});
+		
+
+		document.getElementById("clearall").addEventListener('click',function(){document.getElementById("datareceived").value = '';});
+		
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
