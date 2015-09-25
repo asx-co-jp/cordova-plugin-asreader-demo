a8c6704c370c704764cb48f119751a45c67784d7
Marvin Xue <bsetsu@asx.co.jp>
2015-09-25 22:08:29 +0900

    change indent to TAB

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -17,24 +17,24 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
 		
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		app.receivedEvent('deviceready');
 		var rfidsuccess = function(message) {
 			document.getElementById("deviceready").querySelector('.asr_rf_poweron').setAttribute('style', 'display:block;');
 		}
@@ -129,18 +129,18 @@ var app = {
 
 		document.getElementById("clearall").addEventListener('click',function(){document.getElementById("datareceived").value = '';});
 		
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+		console.log('Received Event: ' + id);
+	}
 };
 
 app.initialize();
---
+++ plugins/com.asreader.cdv/README.md
@@ -0,0 +1,20 @@
+# cordova-plugin-asreader
+cordova plugin for AsReader
+# How to use
+    cordova plugin add https://github.com/asx-co-jp/cordova-plugin-asreader.git
+
+You can find a sample code here
+https://github.com/asx-co-jp/cordova-plugin-asreader-demo
+
+# Issues & Contributions
+This plugin is open for everyone. You are welcome to raise issues here. Bug fixes and pull requests will also be deeply appreciated.
+
+# AsReader
+AsReader is a series of Apple MFi accessory devices for iOS devices, which can read Barcodes, read/write RFID tags and NFC.
+They turn your iOS device into a powerful enterprise level handheld terminal very easily.
+For more information about AsReader please refer to their official website.
+http://asreader.com
+
+
+Enjoy AsReader & Cordova!
+
